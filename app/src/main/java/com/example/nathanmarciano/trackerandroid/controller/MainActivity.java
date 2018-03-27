02e40550f892966e0b27d9fc6e1e200504e21eea
Nathan Marciano <marcianonathan1@gmail.com>
2018-03-27 23:38:07 +0200

    implementing mvc

--- app/src/main/java/com/example/nathanmarciano/trackerandroid/controller/MainActivity.java
+++ app/src/main/java/com/example/nathanmarciano/trackerandroid/controller/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.nathanmarciano.trackerandroid;
+package com.example.nathanmarciano.trackerandroid.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.nathanmarciano.trackerandroid.R;
 
 public class MainActivity extends AppCompatActivity {
 
